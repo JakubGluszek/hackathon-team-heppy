86236418e62a932155c9def4d81dbfec089b9438
HajMichal <michalhaj2003@gmail.com>
2025-11-08 11:29:34 +0100

    Repo ready to go

--- cognify/src/server/api/routers/post.ts
+++ cognify/src/server/api/routers/post.ts
@@ -3,19 +3,10 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
-  publicProcedure,
 } from "@/server/api/trpc";
 import { posts } from "@/server/db/schema";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: protectedProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -31,9 +22,5 @@ export const postRouter = createTRPCRouter({
     });
 
     return post ?? null;
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
   }),
 });
